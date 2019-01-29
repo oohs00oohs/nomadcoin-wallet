ec992ab8763ecaf234cea3f75f7a1b23a066a696
tndyd5390 <tndyd5390@naver.com>
2019-01-29 10:44:08 +0900

    styled-components 버전 다운그레이드함

--- uidev/src/Components/App/AppContainer.js
+++ uidev/src/Components/App/AppContainer.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { createGlobalStyle } from "styled-components";
+import { injectGlobal } from "styled-components";
 import reset from "styled-reset";
 import axios from "axios";
 import typography from "../../typography";
-import { MASTER_NODE, SELF_NODE, SELF_P2P_NODE} from "../../constants";
+import { MASTER_NODE, SELF_NODE, SELF_P2P_NODE } from "../../constants";
 import AppPresenter from "./AppPresenter";
-import { async } from "rxjs/internal/scheduler/async";
 
-const baseStyles = () => createGlobalStyle`
+const baseStyles = () => injectGlobal`
   ${reset};
   ${typography};
   h1,h2,h3,h4{
@@ -19,58 +18,79 @@ const baseStyles = () => createGlobalStyle`
 class AppContainer extends Component {
   state = {
     isLoading: true,
-    isMining: false
+    isMining: false,
+    toAddress: "",
+    amount: "0"
   };
   static propTypes = {
     sharedPort: PropTypes.number.isRequired
-  }
+  };
   componentDidMount = () => {
-    const {sharedPort} = this.props;
+    const { sharedPort } = this.props;
     this._registerOnMaster(sharedPort);
-    this._getAddress(sharedPort);
     this._getBalance(sharedPort);
+    this._getAddress(sharedPort);
     setInterval(() => this._getBalance(sharedPort), 1000);
-  }
-
+  };
   render() {
     baseStyles();
-    return <AppPresenter {...this.state} mineBlock={this._mineBlock}/>;
+    return (
+      <AppPresenter
+        {...this.state}
+        mineBlock={this._mineBlock}
+        handleInput={this._handleInput}
+        handleSubmit={this._handleSubmit}
+      />
+    );
   }
-
   _registerOnMaster = async port => {
     const request = await axios.post(`${MASTER_NODE}/peers`, {
-      peer : SELF_P2P_NODE(port)
+      peer: SELF_P2P_NODE(port)
     });
   };
-
-  _getAddress = async (port) => {
+  _getAddress = async port => {
     const request = await axios.get(`${SELF_NODE(port)}/me/address`);
     this.setState({
       address: request.data,
-      isLoading : false
-    })
+      isLoading: false
+    });
   };
-
-  _getBalance = async(port) => {
+  _getBalance = async port => {
     const request = await axios.get(`${SELF_NODE(port)}/me/balance`);
-    const {balance} = request.data;
-
+    const { balance } = request.data;
     this.setState({
       balance
-    })
+    });
   };
-
-  _mineBlock = async() => {
+  _mineBlock = async () => {
     const { sharedPort } = this.props;
     this.setState({
       isMining: true
-    })
+    });
     const request = await axios.post(`${SELF_NODE(sharedPort)}/blocks`);
     this.setState({
       isMining: false
-    })
-
-  }
+    });
+  };
+  _handleInput = e => {
+    const { target: { name, value } } = e;
+    this.setState({
+      [name]: value
+    });
+  };
+  _handleSubmit = async e => {
+    e.preventDefault();
+    const { sharedPort } = this.props;
+    const { amount, toAddress } = this.state;
+    const request = await axios.post(`${SELF_NODE(sharedPort)}/transactions`, {
+      amount: Number(amount),
+      address: toAddress
+    });
+    this.setState({
+      amount: "",
+      toAddress: ""
+    });
+  };
 }
 
 export default AppContainer;
--- uidev/src/Components/App/AppPresenter.js
+++ uidev/src/Components/App/AppPresenter.js
@@ -1,14 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import {
-  Card,
-  Key,
-  KeyName,
-  Title,
-  Notification,
-  Button
-} from "Components/Shared";
+import { Card, Key, KeyName, Title, Button } from "Components/Shared";
 
 const Header = styled.div`
   margin: 50px 0;
@@ -16,8 +9,7 @@ const Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
-
+`;
 
 const AppContainer = styled.div`
   background-color: #f2f6fa;
@@ -27,27 +19,89 @@ const AppContainer = styled.div`
   flex-direction: column;
 `;
 
-const AppPresenter = ({ 
+const SendTxForm = styled.form`
+  margin-top: 25px;
+`;
+
+const Submit = Button.withComponent("input").extend`
+  margin-right:10px;
+  border: 2px solid #305371;
+  box-shadow:none;
+  &:hover{
+      box-shadow:none;
+      transform:none;
+  }
+  &:disabled{
+      color:#999;
+      border: 2px solid #999;
+      cursor:not-allowed;
+      box-shadow:none;
+  }
+`;
+
+const Input = Submit.extend`
+  width: 200px;
+  padding-left: 10px;
+  &:active {
+    background-color: transparent;
+  }
+  color: ${props => (props.hasError ? "#e74c3c" : "inherit")};
+  border-color: ${props => (props.hasError ? "#e74c3c" : "inherit")};
+`;
+
+const AppPresenter = ({
   isLoading,
-  address="",
-  balance="",
+  address = "",
+  balance = "",
   mineBlock,
-  isMining
+  isMining,
+  toAddress = "",
+  amount = 0,
+  handleInput,
+  handleSubmit
 }) => (
   <AppContainer>
     <Header>
       <Title>{isLoading ? "Loading..." : "Nomadcoin Wallet"}</Title>
       <Button disabled={isMining} onClick={mineBlock}>
-        {isMining ? "Minging" : "Mine"}
+        {isMining ? "Mining" : "Mine"}
       </Button>
     </Header>
     <Card>
       <Key>
-        <KeyName>Your address : </KeyName><Key>{address}</Key>
+        <KeyName>Your address:</KeyName> {address}
       </Key>
       <Key>
-        <KeyName>Your balance</KeyName> {balance} NMD
+        <KeyName>Your balance:</KeyName> {balance} NMD
       </Key>
+    </Card>
+    <Card>
+      <Key>Send NMD: </Key>
+      <SendTxForm onSubmit={handleSubmit}>
+        <Input
+          placeholder={"Address"}
+          required
+          name="toAddress"
+          value={toAddress}
+          type={"text"}
+          onChange={handleInput}
+        />
+        <Input
+          placeholder={"Amount"}
+          required
+          name="amount"
+          type={"number"}
+          value={amount || ""}
+          onChange={handleInput}
+          max={balance}
+        />
+        <Submit
+          value={"Send"}
+          type={"submit"}
+          readOnly
+          disabled={!toAddress || !amount}
+        />
+      </SendTxForm>
     </Card>
   </AppContainer>
 );
@@ -57,7 +111,11 @@ AppPresenter.propTypes = {
   address: PropTypes.string,
   balance: PropTypes.number,
   mineBlock: PropTypes.func.isRequired,
-  isMining: PropTypes.bool.isRequired
-}
+  isMining: PropTypes.bool.isRequired,
+  toAddress: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  handleInput: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
 
 export default AppPresenter;
